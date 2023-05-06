fab2c5484d424fa2af8913998f63bc8c6c8b52de
Jarkko Niemi <jarkko.niemi@student.hamk.fi>
2023-05-06 13:23:16 +0300

    Added avg calculation to topics

--- feedback/admin.py
+++ feedback/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from .models import Topic, Feedback
+from django.db.models import Avg
 
 class TopicAdmin(admin.ModelAdmin):
-    list_display = ('name',)
+    list_display = ('name', 'display_average_rating')
     search_fields = ['name']
+
+    def display_average_rating(self, obj):
+        average_rating = Feedback.objects.filter(topic=obj).aggregate(Avg('rating'))['rating__avg']
+        return f"{average_rating:.2f}" if average_rating else "N/A"
+    display_average_rating.short_description = 'Keskiarvo'
 
 class FeedbackAdmin(admin.ModelAdmin):
     list_display = ('topic', 'rating', 'good', 'bad', 'ideas', 'date')
--- feedback/models.py
+++ feedback/models.py
@@ -22,3 +22,4 @@ class Feedback(models.Model):
 
     def __str__(self):
         return f"{self.date}"
+
